78b7d7a3e569c5e36adc7abfc4adf64a787e68a6
Grey <81944598+Grey03@users.noreply.github.com>
2021-11-18 15:43:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import math
+global quitconsole
+quitconsole = False
+
+#               ***Instructions for new commands***
+#First put the name of the command in the basiccommands array
+#Then go the the command section and add what you want
+
+#                         ***COMMANDSSS***
+#These are your commands just put the name of it here then look below for other info
+global basiccommands
+basiccommands = ["help","quit","clear"]
+
+#                         ***Interpretor***
+#Take your command and searches through command list for it
+def commandinterp(a):
+  commandfound = False
+  global basiccommands
+  for i in range(len(basiccommands)):
+    if commandfound == False and a.startswith(basiccommands[i] + " "):
+        commandfound = True
+        return i
+  if commandfound == False:
+    print ("\" " + str(a) + "\" is an Invalid Command\n")
+
+while (quitconsole != True):
+  comipt = ((input("Enter your command > ")).lower() + " ")
+
+  #                    ***COMMAND SECTION***
+  #simply make an if for  your command that leads to what you want 
+  def commandrun(command):
+    if command == 0:
+      print (basiccommands)
+    if command == 1:
+      quitconsole = True
+    if command == 2:
+      for l in range(0,200):
+        print ("")
+
+  commandrun(commandinterp(comipt))
