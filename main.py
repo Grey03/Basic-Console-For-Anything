e5a1dfc3a3331a8857cf2bc04361c26705a7d03b
Grey <81944598+Grey03@users.noreply.github.com>
2021-11-21 03:25:03 +0000

    Reorganized some moreeeee, should be easier to add commands now.

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ global inpt
 inpt = ""
 # --------------STEPS FOR COMMANDSSSS------------
 # 1. GO TO note labeled "1." Then just add your command to the end of the list
-# 2. GO TO note labeled "2." Then just add what your command does
+# 2. GO TO note labeled "2." Then just add a description for your command
+# 3. GO TO note labeled "3." Then just add what your command does
 
 global commands
 #               1. (Add ur command at the end)
-commands = ["commands","quitconsole","clearconsole","varexample"]
+commands = ["commands",
+"quitconsole",
+"clearconsole",
+"help",
+"varexample"]
+
+
+#               2. (Add a brief desc)
+global commanddesc
+commanddesc = ["Shows list of all commands",
+"Quits the console",
+"Clears the console",
+"Shows descriptions of commands",
+"This is just an example but it takes 2 numbers and adds them and shows you the sum"]
 
 
 def consolestart():
@@ -38,6 +52,7 @@ def quit():
 
 def runcommand():
   global commands
+  global commanddesc
   global quitconsole
   global inpt
   commandnum = -1
@@ -47,7 +62,7 @@ def runcommand():
       validcommand = True
       commandnum = i
 
-  #             2. (add an elif with the # of the spot u put ur new command)
+  #             3. (add an elif with the # of the spot u put ur new command)
   # Look below for examples of what u could do
   if commandnum == 0:
     print ("----------")
@@ -62,6 +77,17 @@ def runcommand():
     for i in range(200):
       print ("\n")
   elif commandnum == 3:
+    print ("Enter the name of the command you want info of")
+    whichcommand = input("> ")
+    foundit = False
+    for i in range(len(commanddesc)):
+      if whichcommand == commands[i]:
+        print (commanddesc[i])
+        foundit = True
+    if foundit == False:
+      print ("That command name doesnt exist")
+  
+  elif commandnum == 4:
     #THIS IS A VAR EXAMPLE FOR COMMANDS THAT NEED EXTRA JUNK
     #Just add ur inputs with the command
     print ("Enter First Num")
@@ -70,7 +96,7 @@ def runcommand():
     y = int(input("> "))
     print ("x + y = " + str(x+y))
   else:
-    print ("\"" + str(inpt) + "\" is an invalid command!")
+    print ("\"" + str(inpt) + "\" is an invalid command! Just enter the name of the command, nothing more.")
   print ("")
 
 consolestart()
